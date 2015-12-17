5b547cbf5e3519496c28c38e3688df2ec4615277
Camilo Roca <carocad@unal.edu.co>
2015-12-17 16:14:45 -0500

    link-matrix and find-sequence functions modified to accept default arguments

--- src/frechet_dist/shared.clj
+++ src/frechet_dist/shared.clj
@@ -1,7 +1,6 @@
 (ns frechet-dist.shared
-  (:require [clojure.core.matrix :refer [matrix get-row row-count mget
-                                         emax mset! compute-matrix shape
-                                         distance]]))
+  (:require [clojure.core.matrix :refer [get-row row-count mget emax
+                                         mset! compute-matrix shape]]))
 
 (defn max-leash
   "base on the distance matrix CA find the maximum distance using the provided
@@ -20,7 +19,9 @@
   "Given a point2point distance matrix CA find the path enclosed by the limits
   i-start j-start i-end j-end that minimizes the distance between the two
   curves from which CA was created."
-  [CA [i-start j-start i-end j-end]]
+  ([CA]
+   (find-sequence CA (apply conj [0 0] (shape CA))))
+  ([CA [i-start j-start i-end j-end]]
   (loop [i (dec i-end)
          j (dec j-end)
          path (transient [])]
@@ -35,14 +36,18 @@
          (cond
           (and (>= left diag) (>= top diag)) (recur (dec i) (dec j) (conj! path [i j]))
           (and (>= diag left) (>= top left)) (recur (dec i) j (conj! path [i j]))
-          (and (>= diag top) (>= left top)) (recur i (dec j) (conj! path [i j])))))))
+          (and (>= diag top) (>= left top)) (recur i (dec j) (conj! path [i j]))))))))
 
 (defn link-matrix
   "calculate the frechet distance among all possible discrete parametrization
   of the curves P and Q using dist-fn. The discrete frechet distance is
   returned along the coupling sequence."
-  [p2p-dist [i-start j-start i-end j-end]]
-  (let [CA         (compute-matrix :vectorz [i-end j-end] (fn [i j] -1))
+  ([p2p-dist]
+   (link-matrix p2p-dist (apply conj [0 0] (shape p2p-dist))))
+  ([p2p-dist [i-start j-start i-end j-end]]
+   ;NOTE: the size of the matrix is kept equal to p2p-dist matrix in order
+  ; to get the right index for the coupling sequence when the limits passed
+  (let [CA         (compute-matrix :vectorz (shape p2p-dist) (fn [i j] -1))
         cd-fn      (fn cd [i j] ;cd : calculate distance
                      (cond
                       (> (mget CA i j) -1) :default ; do nothing
@@ -53,6 +58,7 @@
                                                                                 (cd (dec i) (dec j))
                                                                                 (cd i (dec j)))
                                                                            (mget p2p-dist i j))))
-                     (mget CA i j)) ;shape is 1-indexed but mget is zero-indexed
+                     (mget CA i j))
+        ;shape is 1-indexed but mget is zero-indexed
         dist       (cd-fn (dec i-end) (dec j-end))]
-    [dist CA]))
+    [dist CA])))
