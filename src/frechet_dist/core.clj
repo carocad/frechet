80788ff2cf65215799c14c5f0311c8cb4d7e0862
Camilo Roca <carocad@unal.edu.co>
2015-12-23 12:44:36 -0500

    cosmetic changes

--- src/frechet_dist/core.clj
+++ src/frechet_dist/core.clj
@@ -3,20 +3,6 @@
             [frechet-dist.partial :refer [relax-boundaries]]
             [frechet-dist.shared :refer [link-matrix find-sequence point-distance]] :reload))
 
-(defn partial-frechet-dist
-  "compute the partial frechet distance among P and Q. The partial distance is
-  calculated as the frechet distance among R and T, where R and T are R and T
-  are the longest continous subcurves from P and Q that minimize the frechet
-  distance."
-  ([P Q]
-   (partial-frechet-dist P Q distance))
-  ([P Q dist-fn]
-   (let [p2p-dist      (point-distance P Q dist-fn)
-         limits        (relax-boundaries p2p-dist)
-         [dist CA]     (link-matrix p2p-dist limits)
-         coupling      (find-sequence CA limits)]
-     [dist coupling])))
-
 (defn frechet-dist
   "calculate the discrete frechet distance between two curves.
   P and Q can be MxD and NxD matrixes. This means that both MUST
@@ -31,3 +17,17 @@
         [dist CA]  (link-matrix p2p-dist)
         coupling   (find-sequence CA)]
     [dist coupling])))
+
+(defn partial-frechet-dist
+  "compute the partial frechet distance among P and Q. The partial distance is
+  calculated as the frechet distance among R and T, where R and T are R and T
+  are the longest continous subcurves from P and Q that minimize the frechet
+  distance."
+  ([P Q]
+   (partial-frechet-dist P Q distance))
+  ([P Q dist-fn]
+   (let [p2p-dist      (point-distance P Q dist-fn)
+         limits        (relax-boundaries p2p-dist)
+         [dist CA]     (link-matrix p2p-dist limits)
+         coupling      (find-sequence CA limits)]
+     [dist coupling])))
