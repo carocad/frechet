7bc86bcc9f3c99d77550491caad885bac596f542
Camilo Roca <carocad@unal.edu.co>
2017-01-04 18:41:59 +0100

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject frechet-dist "0.9.0"
+(defproject frechet-dist "0.10.0"
   :description "Calculate the discrete Frechet distance between two polygonal curves"
   :url "https://github.com/carocad/frechet-dist"
   :license {:name "LGPL v3"
