9b6ad8b67832b11b334da90d181f2ad2ac3ae99a
Camilo Roca <roc1fr@bosch.com>
2021-08-27 16:21:37 +0200

    chore: cosmetic changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject frechet-dist "0.11.1"
+(defproject frechet-dist "0.11.2"
   :description "Calculate the discrete Frechet distance between two polygonal curves"
   :url "https://github.com/carocad/frechet-dist"
   :license {:name "LGPL v3"
