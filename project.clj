a0224af5177b51fb9d1a12036fee4788c36e71cf
Camilo Roca <roc1fr@bosch.com>
2021-08-27 16:23:25 +0200

    chore: bump dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [net.mikera/vectorz-clj "0.43.1" :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[criterium "0.4.5"]
-                                  [org.clojure/test.check "0.9.0"]]
+                                  [org.clojure/test.check "0.10.0"]]
                    :test-selectors {:default     (fn [m] (not (some #{:benchmark} (keys m))))
                                     :benchmark   :benchmark}
-                   :plugins [[jonase/eastwood "0.2.3"]]}})
+                   :plugins [[jonase/eastwood "0.3.5"]]}})
