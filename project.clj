6a72a756df383db5cca2e80676b7444823f5a73d
Camilo Roca <roc1fr@bosch.com>
2021-08-31 18:52:31 +0200

    chore: remove vectorz dependency

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "https://github.com/carocad/frechet-dist"
   :license {:name "LGPL v3"
             :url  "https://github.com/carocad/frechet-dist/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [net.mikera/vectorz-clj "0.43.1" :exclusions [org.clojure/clojure]]]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]]
   :profiles {:dev {:dependencies [[criterium "0.4.5"]
                                   [org.clojure/test.check "0.10.0"]]
                    :test-selectors {:default     (fn [m] (not (some #{:benchmark} (keys m))))
