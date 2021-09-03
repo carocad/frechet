91f18edc8ce82c4c6f2e214c074a153b6b46686a
Camilo Roca <roc1fr@bosch.com>
2021-09-03 19:18:44 +0200

    chore: update .travis.yml to run cljs tests in node.js

--- test/carocad/frechet/test_runner.cljs
+++ test/carocad/frechet/test_runner.cljs
@@ -4,6 +4,7 @@
             [carocad.frechet.core-test :as core]
             [carocad.frechet.partial-test :as partial]))
 
+;; https://clojurescript.org/guides/quick-start#running-clojurescript-on-node.js
 (defn -main []
   (enable-console-print!)
   (test/run-all-tests #"carocad.frechet.+"))
